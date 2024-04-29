2e94813646c1a235339ff2ae063c6ca2746e7a32
katikelaashwini <148527437+katikelaashwini@users.noreply.github.com>
2024-04-29 11:58:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 docker-compose up -d
 docker login -u katikelaashwini123 -p dckr_pat_SAOYg8xl-gP5bz_impvevY8Hq60
-docker tag reactapp katikelaashwini123/yashuproject-prod
-docker push katikelaashwini123/yashuproject-prod
+docker tag reactapp katikelaashwini123/yashuproject-dev
+docker push katikelaashwini123/yashuproject-dev
